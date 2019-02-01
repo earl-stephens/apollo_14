b5519458655900d8307d9887c8760a4498bcbc17
earl-stephens <stephensearl@hotmail.com>
2019-02-01 10:59:17 -0700

    started work on user story 3

--- spec/features/astro_page_info_spec.rb
+++ spec/features/astro_page_info_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe "user sees astronaut page info", type: :feature do
 
       expect(page).to have_content("Average Age: 40")
     end
+
+  context "user sees alphabetical listing of missions"
+    it "show each astronauts missions" do
+      astro1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+      mission1 = astro1.missions.create(name: "Gemini 7")
+      mission2 = astro1.missions.create(name: "Capricorn 4")
+      mission3 = astro1.missions.create(name: "Apollo 13")
+
+      visit '/astronauts'
+
+      expect(page).to have_content("Apollo 13 Capricorn 4 Gemini 7")
+    end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,16 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Class Methods' do
+    it "should calculate average age" do
+      astro1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+      astro2 = Astronaut.create(name: "John Glenn", age: 42, job: "Pilot")
+      astro3 = Astronaut.create(name: "Sally Ride", age: 41, job: "Specialist")
+
+      avg = Astronaut.average(:age)
+
+      expect(avg).to eq(40)
+    end
+  end
 end
