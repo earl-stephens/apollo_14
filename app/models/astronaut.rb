7718f49bd1a25a8a80f50ebaec767ba6d7b8b56d
earl-stephens <stephensearl@hotmail.com>
2019-02-01 10:49:54 -0700

    completed user story 2 and tests pass

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -4,4 +4,9 @@ class Astronaut < ApplicationRecord
 
   has_many :astronaut_missions
   validates_presence_of :name, :age, :job
+
+  def self.avg_age
+    self.average(:age)
+  end
+
 end
--- spec/features/astro_page_info_spec.rb
+++ spec/features/astro_page_info_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe "user sees astronaut page info", type: :feature do
       expect(page).to have_content("Age: #{astro.age}")
       expect(page).to have_content("Job: #{astro.job}")
     end
+
+  context "user sees average astronaut age"
+    it "calculates and displays average astronaut age" do
+      astro1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+      astro2 = Astronaut.create(name: "John Glenn", age: 42, job: "Pilot")
+      astro3 = Astronaut.create(name: "Sally Ride", age: 41, job: "Specialist")
+
+      visit '/astronauts'
+
+      expect(page).to have_content("Average Age: 40")
+    end
 end
